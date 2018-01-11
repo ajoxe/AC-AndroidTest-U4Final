1e1ac7b2937770e5605fc3f5c6cb127e986a6d00
Amirah Oxendine <amirahoxendine@ac.c4q.nyc>
2018-01-10 20:10:09 -0500

    added info toggle

--- app/src/main/java/nyc/c4q/androidtest_unit4final/MainActivity.java
+++ app/src/main/java/nyc/c4q/androidtest_unit4final/MainActivity.java
@@ -1,12 +1,14 @@
 package nyc.c4q.androidtest_unit4final;
 
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -20,11 +22,13 @@ public class MainActivity extends AppCompatActivity {
     protected List<String> colorsList;
     View.OnClickListener colorClickListener;
     private static String TAG = "OnClick";
+    LinearLayout fragmentContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fragmentContainer = (LinearLayout) findViewById(R.id.fragment_container);
 
         colorDict = new HashMap<>();
         colorDict.put("indigo", "#4b0082");
@@ -62,6 +66,25 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         return colorClickListener;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.info_options_menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //return super.onOptionsItemSelected(item);
+        if (fragmentContainer.getVisibility() == View.VISIBLE){
+            fragmentContainer.setVisibility(View.GONE);
+        } else {
+            fragmentContainer.setVisibility(View.VISIBLE);
+        }
+        return true;
     }
 
     // TODO: Add options menu with the item "Info" which is always visible
